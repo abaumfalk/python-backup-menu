03089c446bd9a33b22a9c260e9d8f1b7839859c7
Arno Baumfalk <a.baumfalk@web.de>
2023-01-20 13:34:08 +0100

    added missing quotes

--- backup.sh
+++ backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-DIR=$(cd $(dirname $0) && pwd)
+DIR=$(cd "$(dirname "$0")" && pwd)
 
 if [ ! -e "${DIR}/venv" ]; then
-    ${DIR}/make_virtualenv.sh "${DIR}/venv"
+    "${DIR}/make_virtualenv.sh" "${DIR}/venv"
 fi
 
 source "${DIR}/venv/bin/activate"
